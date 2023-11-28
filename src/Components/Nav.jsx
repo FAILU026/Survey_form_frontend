8ff04f8fc99378f8fc8426882e2d923f40b43156
Failur rahuman <100862900+FAILU026@users.noreply.github.com>
2023-11-28 10:00:25 +0530

    Update Nav.jsx

--- src/Components/Nav.jsx
+++ src/Components/Nav.jsx
@@ -7,7 +7,7 @@ const Nav = () => {
     <div className='nav_container'> 
     <nav>
     <Link to="/data"><button className='btn-d'>Data</button></Link>
-    <h1 className='title'>Survey Form</h1>
+   <Link to="/"><h1 className='title'>Survey Form</h1></Link> 
     <Link to="/form"> <button className='btn-f'>Form</button></Link>
       </nav>
       </div>
